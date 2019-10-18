85a26abb43c0ae2d66dfe2f96c2cdd69f35ae187
Dowland Aiello <mitsukomegumii@gmail.com>
2019-10-18 17:26:16 -0400

    immutable: What in god's name have I done?

--- list.go
+++ list.go
@@ -5,8 +5,11 @@ package immutable
 // List is a list.
 type List = ListNode
 
+// Iterator if a generic method used to meaningfully traverse a list.
+type Iterator = func(index int, value interface{})
+
 // ListNode is a node in a list.
-type ListNode func(index int) interface{}
+type ListNode func(interface{}) interface{}
 
 /* BEGIN EXPORTED METHODS */
 
@@ -27,20 +30,33 @@ func NewList(values ...interface{}) List {
 // newListNode initializes a new list node from the given value and index in an
 // overall list.
 func newListNode(index int, value interface{}, children ...interface{}) ListNode {
-	return func(i int) interface{} {
-		// Check is addressed node
-		if index == i {
-			return value // Return the value
+	return func(f interface{}) interface{} {
+		// Check no more children
+		if len(children) == 0 {
+			return nil // Stop execution
 		}
 
-		return newListNode(index+1, children[0], children[1:]...)(i) // Return the following nodes
+		// Inputted parameter is an integer
+		if i, ok := f.(int); ok {
+			if i == index {
+				return value // Return the value of the node
+			}
+		}
+
+		// Inputted parameter is an iterator
+		if i, ok := f.(Iterator); ok {
+			i(index, value) // Call the iterator
+		}
+
+		return newListNode(index+1, children[0], children[1:])(f) // Do for the rest of the nodes
 	} // Return the list node
 }
 
-// nilListNode initializes a zero-value list node.
+// nilListNode makes a list of size 0.
 func nilListNode() ListNode {
-	return func(i int) interface{} {
-		return nil // Always nil
+	// A node of nil value with no children
+	return func(f interface{}) interface{} {
+		return nil // Return nil
 	}
 }
 
