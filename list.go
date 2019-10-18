351381074e4c20f11819eee0f5ea5c9b836b4c33
Dowland Aiello <mitsukomegumii@gmail.com>
2019-10-18 19:31:22 -0400

    immutable: Begin reimplementing list.

--- list.go
+++ list.go
@@ -2,77 +2,58 @@
 // collections.
 package immutable
 
-// List is a list.
-type List = ListNode
+// List is an expandable, immutable data collection.
+type List func(index int) interface{}
 
-// Iterator if a generic method used to meaningfully traverse a list.
-type Iterator = func(index int, value interface{}) interface{}
-
-// ListNode is a node in a list.
-type ListNode func(interface{}) interface{}
+// Pair is a logical pair of interfaces.
+type Pair func() (interface{}, interface{})
 
 /* BEGIN EXPORTED METHODS */
 
-// NewList initializes a new list from the given values.
-func NewList(values ...interface{}) List {
-	// Check list should be empty
-	if len(values) == 0 {
-		return nilListNode() // Return a nil list node
+// NewList initializes a new list from the given set of elements.
+func NewList(elements ...interface{}) List {
+	// Check list is empty
+	if len(elements) == 0 {
+		return NewList(nil) // Make an empty list
 	}
 
-	return newListNode(0, values[0], values[1:]...) // Return the list
+	return makeList(0, nil, elements...) // Make the list
 }
 
 // Size gets the size of the list.
 func (l *List) Size() int {
-	return (*l)(func(index int, value interface{}) interface{} {
-		return index + 1 // Return the index
-	}).(int) // Return the size of the list
-}
-
-// Push pushes an item to the list
-func (l *List) Push(e interface{}) List {
-
+	return l
 }
 
 /* END EXPORTED METHODS */
 
 /* BEGIN INTERNAL METHODS */
 
-// newListNode initializes a new list node from the given value and index in an
-// overall list.
-func newListNode(index int, value interface{}, children ...interface{}) ListNode {
-	return func(f interface{}) interface{} {
-		// Inputted parameter is an integer
-		if i, ok := f.(int); ok {
-			if i == index {
-				return value // Return the value of the node
+// makeList generates a list from the given slice of elements.
+func makeList(index int, list List, elements ...interface{}) List {
+	l := func(i int) interface{} {
+		if i < 0 { // Check out of bounds
+			if i == -1 { // Check is identity
+				return func() (interface{}, interface{}) {
+					return index, elements[0] // Return the index and the element
+				}
 			}
-		} else if i, ok := f.(Iterator); ok {
-			if r := i(index, value); len(children) == 0 { // Check no more children
-				return r // Return the result of the iterator
-			}
+			panic("index out of bounds") // Panic
 		}
 
-		var nextChildren []interface{} = nil // The children passed into the next iteration
-
-		// Check no more children
-		if len(children) == 0 {
-			return nil // Stop execution
-		} else if len(children) > 1 {
-			nextChildren = children[1:] // Has next children
+		if i == index { // Check is node being addressed
+			return elements[0] // Return the element
 		}
 
-		return newListNode(index+1, children[0], nextChildren...)(f) // Do for the rest of the nodes
-	} // Return the list node
-}
-
-// nilListNode makes a list of size 0.
-func nilListNode() ListNode {
-	// A node of nil value with no children
-	return func(f interface{}) interface{} {
-		return nil // Return nil
+		return list(i) // Return the element at that index elsewhere in the list
 	}
+
+	// Check is done constructing the list
+	if len(elements) == 1 {
+		return l // Return the final list
+	}
+
+	return makeList(index+1, l, elements[1:]...) // Return the list after the rest of the constructing calls
 }
 
 /* END INTERNAL METHODS */
